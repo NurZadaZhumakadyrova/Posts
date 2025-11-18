4bd80e66d39991c58acabdeedbbf6a849651e54f
Zhumakadyrova Nurzada <zhumakadyrova.nurzat081093@gmail.com>
2025-11-18 16:43:53 +0600

    Set up eslint and prettier. Installed the shadcn and axios UI libraries.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css';
+import { Button } from '@/components/ui/button.tsx';
 
 const App = () => {
 
   return (
-    <>
-     Hello!
-    </>
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
   );
 };
 
